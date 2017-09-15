efcc4ce75c21d33ec6dfcd92eba9e70a60d9c5bc
elviswf <elviswf@163.com>
2017-09-15 16:32:32 +0800

    add

--- physics/formula.py
+++ physics/formula.py
@@ -37,17 +37,20 @@ def plotdf(df, fig3d):
 
 
 fig3d = plt.figure().gca(projection='3d')
+# plt.axis('equal')
+# fig3d.set_xlim([100, 1000])
+# fig3d.set_xlim([-200, 3000])
+# fig3d.set_xlim([0, 400])
+
+fig3d.auto_scale_xyz([100, 1000], [-200, 3000], [0, 400])
 for i in range(10):
     plotdf(dfs[i], fig3d)
 
+from pykalman import KalmanFilter
+import numpy as np
 
-
-
-
-
-
-
-
-
-
-
+kf = KalmanFilter(transition_matrices=[[1, 0, 1, 0], [0, 1, 0, 1]], observation_matrices=[[0.1, 0.5], [-0.3, 0.0]])
+measurements = np.asarray([[1, 0], [0, 0], [0, 1]])  # 3 observations
+kf = kf.em(measurements, n_iter=5)
+(filtered_state_means, filtered_state_covariances) = kf.filter(measurements)
+(smoothed_state_means, smoothed_state_covariances) = kf.smooth(measurements)
