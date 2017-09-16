26e1f343140d6ec2f2d1b8c0b7929ad7aba69159
Yawei Liu <yawliu@microsoft.com>
2017-09-16 17:22:18 +0800

    add pingPongUnitey

---
+++ PingPongUnity/pingpong/Assets/Script/destroyMe.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class destroyMe : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (DestroyMe()); 
+	}
+	
+	// Update is called once per frame
+
+
+	IEnumerator DestroyMe()
+	{
+		yield return new WaitForSeconds (12f);
+		Destroy (gameObject); 
+	}
+}
